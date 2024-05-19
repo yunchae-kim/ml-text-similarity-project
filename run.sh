139bf6f8680f8e62d814eb71e45c313226da1f50
yunchae-kim <yunchae.dev@gmail.com>
2024-05-19 18:08:12 -0400

    feat: set up run.sh script
    This commit sets up the run.sh script to build and run the Docker container.
    
    - Created the run.sh script
    - Added commands to build the Docker image
    - Added commands to run the Docker container with mounted volumes
    - Created the necessary directories (dataset and output)

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Build the Docker image
+docker build -t Docker-Project .
+
+# Create the dataset and output directories if they don't exist
+mkdir -p dataset
+mkdir -p output
+
+# Run the Docker container with mounted volumes
+docker run --rm -v $(pwd)/dataset:/usr/local/dataset -v $(pwd)/output:/usr/local/output Docker-Project
